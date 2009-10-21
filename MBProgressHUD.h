059cf8714e62db41c413813cec33303f1fd117ea
Austin Ziegler <austin@halostatue.ca>
2009-10-20 20:16:43 -0400

    Importing 0.31 from zip download

--- MBProgressHUD.h
+++ MBProgressHUD.h
@@ -1,10 +1,10 @@
 //
 //  MBProgressHUD.h
-//  Version 0.1
-//  Created by Matej Bukovinski on 8.4.09.
+//  Version 0.31
+//  Created by Matej Bukovinski on 30.9.09.
 //
 
-// This code is distributed under the terms and conditions of the MIT license. 
+// This code is distributed under the terms and conditions of the MIT license.
 
 // Copyright (c) 2009 Matej Bukovinski
 //
@@ -29,32 +29,73 @@
 #import <UIKit/UIKit.h>
 
 /**
+ * MBProgressHUD operation modes.
+ */
+typedef enum {
+	/** Progress is shown using an UIActivityIndicatorView. This is the default. */
+	MBProgressHUDModeIndeterminate,
+	/** Progress is shown using a MBRoundProgressView. */
+	MBProgressHUDModeDeterminate,
+} MBProgressHUDMode;
+
+/**
+ * Defines callback methods for MBProgressHUD delegates.
+ */
+@protocol MBProgressHUDDelegate
+
+/**
+ * A callback function that is called after the hud was fully hidden from the screen.
+ */
+- (void)hudWasHidden;
+
+@end
+
+/**
+ * A progress view for showing definite progress by filling up a circle (similar to the indicator for building in xcode).
+ */
+@interface MBRoundProgressView : UIProgressView {
+
+}
+
+/**
+ * Create a 37 by 37 pixel indicator.
+ * This is the same size as used by the larger UIActivityIndicatorView.
+ */
+- (id)initWithDefaultSize;
+
+@end
+
+/**
  * Displays a simple HUD window containing a UIActivityIndicatorView and two optional labels for short messages.
  *
- * This is a simple drop-in class for displaying a progress HUD view similar to Apples private UIProgressHUD class. 
- * The MBProgressHUD window spans over the entire space given to it by the initWithFrame constructor and catches all user 
- * input on this region, thereby preventing the user operations on components below the view. 
- * The HUD itself is drawn centered as a rounded semi-transparent view witch resizes depending on the user specified content. 
+ * This is a simple drop-in class for displaying a progress HUD view similar to Apples private UIProgressHUD class.
+ * The MBProgressHUD window spans over the entire space given to it by the initWithFrame constructor and catches all user
+ * input on this region, thereby preventing the user operations on components below the view.
+ * The HUD itself is drawn centered as a rounded semi-transparent view witch resizes depending on the user specified content.
  * This view supports three modes of operation:
- * - The default mode displays just a UIActivityIndicatorView. 
- * - If the labelText property is set and non-empty then a label containing the provided content is placed below the UIActivityIndicatorView. 
- * - If also the detailsLabelText property is set then another label is placed below the firs label. 
+ * - The default mode displays just a UIActivityIndicatorView.
+ * - If the labelText property is set and non-empty then a label containing the provided content is placed below the UIActivityIndicatorView.
+ * - If also the detailsLabelText property is set then another label is placed below the firs label.
  */
 @interface MBProgressHUD : UIView {
+
+	MBProgressHUDMode mode;
 
 	SEL methodForExecution;
 	id targetForExecution;
 	id objectForExecution;
 	bool useAnimation;
-	
+
 	float width;
 	float height;
-	
-	UIActivityIndicatorView *indicator;
+
+	UIView *indicator;
 	UILabel *label;
 	UILabel *detailsLabel;
-	
-	id delegate;
+
+	float progress;
+
+	id<MBProgressHUDDelegate> delegate;
 	NSString *labelText;
 	NSString *detailsLabelText;
 	float opacity;
@@ -64,20 +105,27 @@
 
 /**
  * A convenience constructor tat initializes the HUD with the window's bounds.
- * Calls the designated constructor with window.bounds as the parameter. 
+ * Calls the designated constructor with window.bounds as the parameter.
  */
 - (id)initWithWindow:(UIWindow *)window;
 
-/** 
- * The HUD delegate object. If set the delegate will receive hudWasHidden callbacks when the hud was hidden.  
- * The delegate should conform to the MBProgressHUDDelegate protocol and implement the hudWasHidden method. 
+/**
+ * MBProgressHUD operation mode. Switches between indeterminate (MBProgressHUDModeIndeterminate)
+ * and determinate progress (MBProgressHUDModeDeterminate). The default is MBProgressHUDModeIndeterminate.
  */
-@property (assign) id delegate;
+@property (assign) MBProgressHUDMode mode;
 
 /**
- * An optional short message to be displayed below the activity indicator. 
+ * The HUD delegate object. If set the delegate will receive hudWasHidden callbacks when the hud was hidden.
+ * The delegate should conform to the MBProgressHUDDelegate protocol and implement the hudWasHidden method.
+ * The delegate object will not be retained.
+ */
+@property (assign) id<MBProgressHUDDelegate> delegate;
+
+/**
+ * An optional short message to be displayed below the activity indicator.
  * The HUD is automatically resized to fit the entire text. If the text is too long it will get clipped by displaying "..." at the end.
- * If left unchanged or set to @"", then no message is displayed. 
+ * If left unchanged or set to @"", then no message is displayed.
  */
 @property (copy) NSString *labelText;
 
@@ -88,58 +136,60 @@
 @property (copy) NSString *detailsLabelText;
 
 /**
- * The opacity of the hud window. 
+ * The opacity of the hud window.
  * Defaults to 0.9 (90% opacity).
  */
 @property (assign) float opacity;
 
 /**
  * Font to be used for the main label.
- * Set this property if the default is not adequate. 
+ * Set this property if the default is not adequate.
  */
-@property (assign) UIFont *labelFont;
+@property (retain) UIFont *labelFont;
 
 /**
  * Font to be used for the details label.
- * Set this property if the default is not adequate. 
+ * Set this property if the default is not adequate.
  */
-@property (assign) UIFont *detailsLabelFont;
+@property (retain) UIFont *detailsLabelFont;
 
 /**
- * Shows the HUD while a background task is executing in a new thread, then hides the HUD.
- * 
- * This method also takes care of NSAutoreleasePools so your method does not have to be concerned with setting up a pool.
- *
- * @param method The method to be executed while the HUD is shown. This method will be executed in a new thread. 
- * @param target The object that the target method belongs to. 
- * @param object An optional object to be passed to the method.
- * @param animated If set to YES the HUD will appear and disappear using a fade animation. 
- *        If set to NO the HUD will not use animations while appearing and disappearing. 
+ * The progress of the progress indicator, from 0.0 to 1.0.
+ * Defaults to 0.0.
  */
-- (void)showWhileExecuting:(SEL)method onTarget:(id)target withObject:(id)object animated:(bool)animated;
+@property (assign) float progress;
 
 /**
- * Display the HUD
+ * Display the HUD.
+ * You need to make sure that the main thread completes it's run loop
+ * soon after this method call so the user interface can be updated.
+ * Call this method when your task is already set-up to be executed
+ * in a new thread (e.g., when using something like NSOperation
+ * or calling an asynchronous call like NSUrlRequest).
+ * @param animated If set to YES the HUD will appear using a fade animation.
+ *        If set to NO the HUD will not use animations while appearing.
  */
 - (void) show:(BOOL)animated;
 
 /**
  * Hide the HUD, this still calls the hudWasHidden delegate.
+ * This is the counterpart of the hide: method. Use it to hide the hud when your task completes.
+ * @param animated If set to YES the HUD will disappear using a fade animation.
+ *        If set to NO the HUD will not use animations while disappearing.
  */
 - (void) hide:(BOOL)animated;
 
-@end
-
-
 /**
- * Defines callback methods for MBProgressHUD delegates.
+ * Shows the HUD while a background task is executing in a new thread, then hides the HUD.
+ *
+ * This method also takes care of NSAutoreleasePools so your method does not have to be concerned with setting up a pool.
+ *
+ * @param method The method to be executed while the HUD is shown. This method will be executed in a new thread.
+ * @param target The object that the target method belongs to.
+ * @param object An optional object to be passed to the method.
+ * @param animated If set to YES the HUD will appear and disappear using a fade animation.
+ *        If set to NO the HUD will not use animations while appearing and disappearing.
  */
-@protocol MBProgressHUDDelegate 
-
-/**
- * A callback function that is called after the hud was fully hidden from the screen.
- */
-- (void)hudWasHidden;
+- (void)showWhileExecuting:(SEL)method onTarget:(id)target withObject:(id)object animated:(bool)animated;
 
 @end
-
--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -1,11 +1,10 @@
 //
 //  MBProgressHUD.m
-//  Version 0.1
-//  Created by Matej Bukovinski on 8.4.09.
+//  Version 0.31
+//  Created by Matej Bukovinski on 30.9.09.
 //
 
 #import "MBProgressHUD.h"
-
 
 @interface MBProgressHUD ()
 
@@ -16,6 +15,13 @@
 
 - (void)done;
 
+- (void)updateLabelText:(NSString *)newText;
+- (void)updateDetailsLabelText:(NSString *)newText;
+- (void)updateProgress;
+- (void)updateIndicators;
+
+@property (retain) UIView *indicator;
+
 @property (assign) float width;
 @property (assign) float height;
 
@@ -25,15 +31,80 @@
 
 @implementation MBProgressHUD
 
+#pragma mark Accessors
+
+@synthesize mode;
+
 @synthesize delegate;
 @synthesize labelText;
 @synthesize detailsLabelText;
 @synthesize opacity;
 @synthesize labelFont;
 @synthesize detailsLabelFont;
+@synthesize progress;
+
+@synthesize indicator;
 
 @synthesize width;
 @synthesize height;
+
+- (void)setMode:(MBProgressHUDMode)newMode {
+	// Dont change mode if it wasn't actually changed to pervent flickering
+	if (mode && mode == newMode) return;
+	mode = newMode;
+	[self performSelectorOnMainThread:@selector(updateIndicators) withObject:nil waitUntilDone:NO];
+	[self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:nil waitUntilDone:NO];
+}
+
+- (void)setLabelText:(NSString *)newText {
+	[self performSelectorOnMainThread:@selector(updateLabelText:) withObject:newText waitUntilDone:NO];
+	[self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:nil waitUntilDone:NO];
+}
+
+- (void)setDetailsLabelText:(NSString *)newText {
+	[self performSelectorOnMainThread:@selector(updateDetailsLabelText:) withObject:newText waitUntilDone:NO];
+	[self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:nil waitUntilDone:NO];
+}
+
+- (void)setProgress:(float)newProgress {
+	progress = newProgress;
+	// Update display ony if showind the determinate progress view
+	if (mode == MBProgressHUDModeDeterminate) {
+		[self performSelectorOnMainThread:@selector(updateProgress) withObject:nil waitUntilDone:NO];
+		[self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:nil waitUntilDone:NO];
+	}
+}
+
+#pragma mark Acessor heplers
+
+- (void)updateLabelText:(NSString *)newText {
+	if (labelText != newText) {
+        [labelText release];
+        labelText = [newText copy];
+    }
+}
+
+- (void)updateDetailsLabelText:(NSString *)newText {
+	if (detailsLabelText != newText) {
+        [detailsLabelText release];
+        detailsLabelText = [newText copy];
+    }
+}
+
+- (void)updateProgress {
+	[(MBRoundProgressView *)indicator setProgress:progress];
+}
+
+- (void)updateIndicators {
+	if (indicator) [indicator removeFromSuperview];
+	if (mode == MBProgressHUDModeDeterminate) 
+		self.indicator = [[MBRoundProgressView alloc] initWithDefaultSize];
+	else {
+		self.indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+		[(UIActivityIndicatorView *)indicator startAnimating];
+	}
+	[self addSubview:indicator];
+}
 
 
 #pragma mark Build up
@@ -44,6 +115,8 @@
 #define LABELFONTSIZE 22.0
 #define LABELDETAILSFONTSIZE 16.0
 
+#define PI 3.14159265358979323846
+
 - (id)initWithWindow:(UIWindow* )window {
 	return [self initWithFrame:window.bounds];
 }
@@ -51,6 +124,7 @@
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
 		// Set default values for properties
+		self.mode = MBProgressHUDModeIndeterminate;
 		self.labelText = @"";
 		self.detailsLabelText = @"";
 		self.opacity = 0.9;
@@ -64,20 +138,16 @@
 		// Make invisible for now
 		self.alpha = 0.0;
 		
-		// Add indicator subview
-		indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-
 		// Add label
 		label = [[UILabel alloc] initWithFrame:self.bounds];
 		
 		// Add details label
 		detailsLabel = [[UILabel alloc] initWithFrame:self.bounds];
-		
     }
     return self;
 }
 
-- (void)layoutAndStyle {
+- (void)layoutSubviews {
 	
 	CGRect frame = self.bounds;
 	
@@ -87,12 +157,9 @@
 	self.height = indFrame.size.height + 2*MARGIN;
 	
 	// Center the indicator
-	indFrame.origin.x = (frame.size.width-indFrame.size.width)/2;
-	indFrame.origin.y = (frame.size.height-indFrame.size.height)/2;
+	indFrame.origin.x = floor((frame.size.width-indFrame.size.width)/2);
+	indFrame.origin.y = floor((frame.size.height-indFrame.size.height)/2);
 	indicator.frame = indFrame;
-	
-	[indicator startAnimating];
-	[self addSubview:indicator];
 	
 	// Add label if label text was set
 	if (self.labelText != @"") {
@@ -124,15 +191,15 @@
 		self.height = self.height + lHeight + PADDING; 
 		
 		// Move indicator to make room for the label
-		indFrame.origin.y -= (lHeight/2 + PADDING/2);
+		indFrame.origin.y -= (floor(lHeight/2 + PADDING/2));
 		indicator.frame = indFrame;
 		
 		// Set the label position and dimensions
-		CGRect lFrame = CGRectMake((frame.size.width-lWidth)/2, indFrame.origin.y + indFrame.size.height + PADDING, lWidth, lHeight);
+		CGRect lFrame = CGRectMake(floor((frame.size.width-lWidth)/2), floor(indFrame.origin.y + indFrame.size.height + PADDING), lWidth, lHeight);
 		label.frame = lFrame;
 		
 		[self addSubview:label];
-	
+		
 		// Add details label delatils text was set
 		if (self.detailsLabelText != @"") {
 			
@@ -162,15 +229,15 @@
 			self.height = self.height + lHeight + PADDING; 
 			
 			// Move indicator to make room for the new label
-			indFrame.origin.y -= (lHeight/2 + PADDING/2);
+			indFrame.origin.y -= (floor(lHeight/2 + PADDING/2));
 			indicator.frame = indFrame;
 			
 			// Move first label to make room for the new label
-			lFrame.origin.y -= (lHeight/2 + PADDING/2);
+			lFrame.origin.y -= (floor(lHeight/2 + PADDING/2));
 			label.frame = lFrame;
 			
 			// Set label position and dimensions
-			CGRect lFrameD = CGRectMake((frame.size.width-lWidth)/2, lFrame.origin.y + lFrame.size.height + PADDING, lWidth, lHeight);
+			CGRect lFrameD = CGRectMake(floor((frame.size.width-lWidth)/2), lFrame.origin.y + lFrame.size.height + PADDING, lWidth, lHeight);
 			detailsLabel.frame = lFrameD;
 			
 			[self addSubview:detailsLabel];
@@ -182,9 +249,7 @@
 #pragma mark Showing and execution
 
 - (void) show:(BOOL)animated {
-	[self layoutAndStyle];
 	[self setNeedsDisplay];
-
 	[self showUsingAnimation:animated];
 }
 
@@ -193,8 +258,6 @@
 }
 
 - (void)showWhileExecuting:(SEL)method onTarget:(id)target withObject:(id)object animated:(bool)animated {
-	
-	[self layoutAndStyle];
 	[self setNeedsDisplay];
 	
 	methodForExecution = method;	
@@ -227,7 +290,8 @@
 
 - (void)done {
 	// If delegate was set make the callback
-	if (delegate != nil && [delegate respondsToSelector:@selector(hudWasHidden)]) {
+	self.alpha = 0.0;
+	if (delegate != nil) {
 		[delegate hudWasHidden];
 	} else {
 		[NSException raise:NSInternalInconsistencyException format:@"Delegate doesn't respond to hudWasHidden"];	
@@ -256,13 +320,15 @@
 }
 
 - (void)hideUsingAnimation:(BOOL)animated {
-	// Fade in 
+	// Fade out 
 	if (animated) { 
 		[UIView beginAnimations:nil context:NULL];
 		[UIView setAnimationDuration:0.40];
 		[UIView setAnimationDelegate:self];
 		[UIView setAnimationDidStopSelector:@selector(animationFinished: finished: context:)];
-		self.alpha = 0.0;
+		// 0.02 prevents the hud from passing through touches during the animation
+		// the hud will get completely hidden in the done method
+		self.alpha = 0.02;
 		[UIView commitAnimations];
 	} else {
 		self.alpha = 0.0;
@@ -308,5 +374,38 @@
     [super dealloc];
 }
 
+
+@end
+
+
+@implementation MBRoundProgressView
+
+- (id)initWithDefaultSize {
+	return [super initWithFrame:CGRectMake(0.0f, 0.0f, 37.0f, 37.0f)];
+}
+
+- (void)drawRect:(CGRect)rect {
+	CGRect allRect = self.bounds;
+	CGRect circleRect = CGRectMake(allRect.origin.x + 2, allRect.origin.y + 2, 
+								   allRect.size.width - 4, allRect.size.height - 4);
+	
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	
+	// Draw background
+	CGContextSetRGBStrokeColor(context, 1.0, 1.0, 1.0, 1.0); // white
+	CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 0.1); // translucent white
+	CGContextSetLineWidth(context, 2.0);
+	CGContextFillEllipseInRect(context, circleRect);
+	CGContextStrokeEllipseInRect(context, circleRect);
+	
+	// Draw progress
+	float x =  allRect.size.width / 2;
+	float y =  allRect.size.height / 2;
+	CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 1.0); // white
+	CGContextMoveToPoint(context, x, y);     
+    CGContextAddArc(context, x, y, (allRect.size.width-4)/2, -PI/2, (self.progress*2*PI)-PI/2, 0); 
+    CGContextClosePath(context); 
+    CGContextFillPath(context);
+}
 
 @end
