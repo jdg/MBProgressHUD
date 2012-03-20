45b303c67b49d83572d81085498b67f7690f9c5c
Matej Bukovinski <matej@bukovinski.com>
2012-03-20 15:44:11 +0100

    Library project and workspace added. closes #57
    - added missing framework imports
    - added a short note regarding the static library option to the readme

--- MBProgressHUD.h
+++ MBProgressHUD.h
@@ -26,7 +26,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <CoreGraphics/CoreGraphics.h>
 
 @protocol MBProgressHUDDelegate;
 
