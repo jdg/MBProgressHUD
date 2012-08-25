93fff466fed8024e16c748392b561b2e89d0f45f
Ned Ruggeri <ruggeri@self-loop.com>
2012-08-25 15:50:19 -0700

    Set taskInProgres when beginning to execute a block
    Setting graceTime and using block method caused HUD to never appear.

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -377,7 +377,7 @@ static const CGFloat kDetailsLabelFontSize = 12.f;
 
 - (void)showAnimated:(BOOL)animated whileExecutingBlock:(dispatch_block_t)block onQueue:(dispatch_queue_t)queue
 	 completionBlock:(MBProgressHUDCompletionBlock)completion {
-	
+	self.taskInProgress = YES;
 	self.completionBlock = completion;
 	dispatch_async(queue, ^(void) {
         block();
@@ -385,7 +385,7 @@ static const CGFloat kDetailsLabelFontSize = 12.f;
             [self cleanUp];
         });
     });
-    [self show:animated];
+  [self show:animated];
 }
 
 #endif
