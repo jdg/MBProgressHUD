f1a30d27848ebe65c858354976376426113d335d
Matej Bukovinski <matej@bukovinski.com>
2012-11-13 18:27:48 +0100

    Backward compatible fix for UITextAlignmentCenter deprecation in iOS 6.

--- MBProgressHUD.h
+++ MBProgressHUD.h
@@ -74,6 +74,12 @@ typedef enum {
 #endif
 #endif
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000
+	#define MBLabelAlignmentCenter NSTextAlignmentCenter
+#else
+	#define MBLabelAlignmentCenter UITextAlignmentCenter
+#endif
+
 #if NS_BLOCKS_AVAILABLE
 typedef void (^MBProgressHUDCompletionBlock)();
 #endif
