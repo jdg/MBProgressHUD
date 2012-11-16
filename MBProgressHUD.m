b3add2d7e3fffbedd668f2b1c6ac500c43e0d14e
Don Wilson <robert.don.wilson@gmail.com>
2012-11-16 13:53:27 -0700

    Return a HUD as soon as one is found in HUDForView:

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -153,15 +153,14 @@ static const CGFloat kDetailsLabelFontSize = 12.f;
 }
 
 + (MBProgressHUD *)HUDForView:(UIView *)view {
-	MBProgressHUD *hud = nil;
 	NSArray *subviews = view.subviews;
 	Class hudClass = [MBProgressHUD class];
 	for (UIView *aView in subviews) {
 		if ([aView isKindOfClass:hudClass]) {
-			hud = (MBProgressHUD *)aView;
+			return (MBProgressHUD *)aView;
 		}
 	}
-	return hud;
+	return nil;
 }
 
 + (NSArray *)allHUDsForView:(UIView *)view {
