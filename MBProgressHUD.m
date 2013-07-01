e7b749355c2cb6edfc2c7fecf9b62e5c77db17c8
Matej Bukovinski <matej@bukovinski.com>
2013-07-01 19:35:35 +0200

    Forcing zero label height when text is set to @“”.

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -27,7 +27,7 @@
 	#define MB_TEXTSIZE(text, font) [text length] > 0 ? [text \
 		sizeWithAttributes:@{NSFontAttributeName:font}] : CGSizeZero;
 #else
-	#define MB_TEXTSIZE(text, font) [text sizeWithFont:font];
+	#define MB_TEXTSIZE(text, font) [text length] > 0 ? [text sizeWithFont:font] : CGSizeZero;
 #endif
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
@@ -35,8 +35,8 @@
 		boundingRectWithSize:maxSize options:(NSStringDrawingUsesLineFragmentOrigin) \
 		attributes:@{NSFontAttributeName:font} context:nil].size : CGSizeZero;
 #else
-	#define MB_MULTILINE_TEXTSIZE(text, font, maxSize, mode) [text sizeWithFont:font \
-		constrainedToSize:maxSize lineBreakMode:mode];
+	#define MB_MULTILINE_TEXTSIZE(text, font, maxSize, mode) [text length] > 0 ? [text \
+		sizeWithFont:font constrainedToSize:maxSize lineBreakMode:mode] : CGSizeZero;
 #endif
 
 
