5fb0921dab13a3650928f4d373afeaa836ca0e81
Matej Bukovinski <matej@bukovinski.com>
2014-08-12 07:19:56 +0200

    Updated demo appearance.

--- Demo/Classes/HudDemoViewController.h
+++ Demo/Classes/HudDemoViewController.h
@@ -7,30 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "MBProgressHUD.h"
 
-@interface HudDemoViewController : UIViewController <MBProgressHUDDelegate> {
-	MBProgressHUD *HUD;
 
-	long long expectedLength;
-	long long currentLength;
-}
-
-- (IBAction)showSimple:(id)sender;
-- (IBAction)showWithLabel:(id)sender;
-- (IBAction)showWithDetailsLabel:(id)sender;
-- (IBAction)showWithLabelDeterminate:(id)sender;
-- (IBAction)showWIthLabelAnnularDeterminate:(id)sender;
-- (IBAction)showWithLabelDeterminateHorizontalBar:(id)sender;
-- (IBAction)showWithCustomView:(id)sender;
-- (IBAction)showWithLabelMixed:(id)sender;
-- (IBAction)showUsingBlocks:(id)sender;
-- (IBAction)showOnWindow:(id)sender;
-- (IBAction)showURL:(id)sender;
-
-- (void)myTask;
-- (void)myProgressTask;
-- (void)myMixedTask;
+@interface HudDemoViewController : UIViewController
 
 @end
-
