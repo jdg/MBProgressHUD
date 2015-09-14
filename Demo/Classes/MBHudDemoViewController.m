951568b2c554c4960a70d2c8e56767ea283cd81a
Matej Bukovinski <matej@bukovinski.com>
2015-09-14 09:20:57 +0200

    Determinate mode example.

--- Demo/Classes/MBHudDemoViewController.m
+++ Demo/Classes/MBHudDemoViewController.m
@@ -48,7 +48,7 @@
 		[MBExample exampleWithTitle:@"With label" selector:@selector(labelExample)],
 		[MBExample exampleWithTitle:@"With details label" selector:@selector(detailsLabelExample)],
 		[MBExample exampleWithTitle:@"On window" selector:@selector(indeterminateExample)]],
-	  @[[MBExample exampleWithTitle:@"Determinate mode" selector:@selector(indeterminateExample)],
+	  @[[MBExample exampleWithTitle:@"Determinate mode" selector:@selector(determinateExample)],
 		[MBExample exampleWithTitle:@"Annular determinate mode" selector:@selector(indeterminateExample)],
 		[MBExample exampleWithTitle:@"Bar determinate mode" selector:@selector(indeterminateExample)]],
 	  @[[MBExample exampleWithTitle:@"Custom view" selector:@selector(indeterminateExample)],
@@ -125,11 +125,41 @@
 	});
 }
 
+- (void)determinateExample {
+	MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
+
+	// Set the determinate mode to show task porgress.
+	hud.mode = MBProgressHUDModeDeterminate;
+	hud.label.text = NSLocalizedString(@"Loading...", @"HUD title");
+
+	dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+		// Do something useful in the background and update the HUD periodically.
+		[self doSomeWorkWithProgress];
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[hud hideAnimated:YES];
+		});
+	});
+}
+
 #pragma mark - Tasks
 
 - (void)doSomeWork {
 	// Simulate by just waiting.
 	sleep(3.);
+}
+
+- (void)doSomeWorkWithProgress {
+	// This just increases the progress indicator in a loop.
+	float progress = 0.0f;
+	while (progress < 1.0f) {
+		progress += 0.01f;
+		dispatch_async(dispatch_get_main_queue(), ^{
+			// Instead we could have also passed a reference to the HUD
+			// to the HUD to myProgressTask as a method parameter.
+			[MBProgressHUD HUDForView:self.navigationController.view].progress = progress;
+		});
+		usleep(50000);
+	}
 }
 
 #pragma mark - UITableViewDelegate
