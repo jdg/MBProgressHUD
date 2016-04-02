d802c7c73c24cfd7e8f1501ae910298527275d1c
Matej Bukovinski <matej@bukovinski.com>
2016-04-02 20:16:52 +0200

    Delayed hide test.

--- Demo/HudTests/HudTests.m
+++ Demo/HudTests/HudTests.m
@@ -102,6 +102,39 @@ XCTAssertNil(hud.superview, @"The HUD should not have a superview."); \
     [self waitForExpectationsWithTimeout:5. handler:nil];
 }
 
+#pragma mark - Delay
+
+- (void)testDelayedHide {
+    UIViewController *rootViewController = UIApplication.sharedApplication.keyWindow.rootViewController;
+    UIView *rootView = rootViewController.view;
+
+    self.hideExpectation = [self expectationWithDescription:@"The hudWasHidden: delegate should have been called."];
+
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:rootView animated:NO];
+    hud.delegate = self;
+
+    XCTAssertNotNil(hud, @"A HUD should be created.");
+
+    [hud hideAnimated:NO afterDelay:2];
+
+    MBTestHUDIsVisible(hud, rootView);
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        MBTestHUDIsVisible(hud, rootView);
+    });
+
+    XCTestExpectation *hideCheckExpectation = [self expectationWithDescription:@"Hide check"];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // After the grace time passes, the HUD should still not be shown.
+        MBTestHUDIsHidenAndRemoved(hud, rootView);
+        [hideCheckExpectation fulfill];
+    });
+
+    [self waitForExpectationsWithTimeout:5. handler:nil];
+
+    MBTestHUDIsHidenAndRemoved(hud, rootView);
+}
+
 #pragma mark - Ruse
 
 - (void)testNonAnimatedHudReuse {
