5dcbf5b191017068a6592d1b7a17a1d0bd8ef5d8
Cihat Gündüz <commits@cihatguenduez.de>
2019-04-09 16:46:24 +0200

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "MBProgressHUD",
+    products: [
+        .library(name: "MBProgressHUD", targets: ["MBProgressHUD"])
+    ],
+    targets: [
+        .target(
+            name: "MBProgressHUD",
+            path: ".",
+            exclude: ["Demo"],
+            sources: ["MBProgressHUD.h", "MBProgressHUD.m"]
+        )
+    ]
+)
