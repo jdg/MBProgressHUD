9b5fa499c287066a6998a8ec5e526f783d19ef6e
Bruno Coelho <4brunu@gmail.com>
2019-12-29 11:23:20 +0000

    Complete SPM integration

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
