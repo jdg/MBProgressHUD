61fb89ce30d42f403ce53cde65883dc8cea6d02a
Matej Bukovinski <matej@bukovinski.com>
2020-02-19 12:54:40 +0100

    Update copyright year

--- MBProgressHUD.h
+++ MBProgressHUD.h
@@ -6,7 +6,7 @@
 
 // This code is distributed under the terms and conditions of the MIT license.
 
-// Copyright © 2009-2016 Matej Bukovinski
+// Copyright © 2009-2020 Matej Bukovinski
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
